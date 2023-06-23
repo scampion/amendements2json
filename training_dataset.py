ef081c55f69656ea82f189932a23e82059bf3e4d
Sébastien Campion <sebastien.campion@foss4.eu>
2023-06-23 09:59:22 +0200

    LLM finetuning generator

--- training_dataset.py
+++ training_dataset.py
@@ -1,27 +1,32 @@
 import sys
 import json
 
+
+def get_instruction(group):
+    return f"As member of the european parliament for the political group {group} I amend the submitted article text according to my values"
+
+
+def print_json(a, group):
+    print(json.dumps({'instruction': get_instruction(group), 'input': ' '.join(a['text_original']), 'output': ' '.join(a['text_amended'])}))
+
+
 if __name__ == '__main__':
     file_path = sys.argv[1]
-    data = json.load(open(file_path))
-    for dossier, articles in data.items():
-        for article, edits in articles.items():
-            for a in edits:
-                for author in a.get('authors', []):
-                    group = author['group']
-                    instruction = f"As member of the european parliament for the political group {group}, I propose to amend the text submitted as follows:"
-                    print(json.dumps({'instruction': instruction,
-                                        'input': ' '.join(a['text_original']),
-                                        'output': ' '.join(a['text_amended'])}))
+    if file_path.endswith('.json'):
+        data = json.load(open(file_path))
+        for dossier, articles in data.items():
+            for article, edits in articles.items():
+                for a in edits:
+                    for author in a.get('authors', []):
+                        group = author['group']
+                        print_json(a, group)
 
-    # with open(file_path, "r") as f:
-    #     for i, line in enumerate(f.readlines()):
-    #         a = json.loads(line.rstrip())[0]
-    #         if len(a['text_original']) == 0:
-    #             continue
-    #         for author in a['authors']:
-    #             group = author['group']
-    #             instruction = f"As member of the european parliament for the political group {group}, I propose to amend the text submitted as follows:"
-    #             print(json.dumps({'instruction': instruction,
-    #                                 'input': ' '.join(a['text_original']),
-    #                                 'output': ' '.join(a['text_amended'])}))
+    elif file_path.endswith('.jsonl'):
+        with open(file_path, "r") as f:
+            for i, line in enumerate(f.readlines()):
+                a = json.loads(line.rstrip())[0]
+                if len(a['text_original']) == 0:
+                    continue
+                for author in a['authors']:
+                    group = author['group']
+                    print_json(a, group)
